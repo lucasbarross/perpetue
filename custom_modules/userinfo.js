ee7efda95e9c7992c93b542b199af363f32b7ab3
Lucas Barros <lbam@cin.ufpe.br>
2018-01-03 23:31:59 -0300

    Debug IP

--- custom_modules/userinfo.js
+++ custom_modules/userinfo.js
@@ -5,7 +5,8 @@ var hashids = new Hashids('Perpetue');
 var genres = ["male", "female"];
 
 function generateUser(ip){
-    console.log("Before, in user module: " + ip);
+    ip.replace(".", "");
+    console.log(ip);
     let ipHash = hashids.encode(parseInt(ip));
     console.log("After hashid, in user module: " + ipHash);
     let genre = genres[parseInt(ip[8])%2];
